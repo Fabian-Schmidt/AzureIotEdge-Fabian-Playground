435dc2fc68d96822afd62cb1dcee2080500c6e35
Fabian Schmidt <Fabian.Schmidt@TeamSensei.com.au>
2018-09-02 20:33:47 +1000

    Upgrade moduel to GA

--- DataReaderDHT22/Program.cs
+++ DataReaderDHT22/Program.cs
@@ -19,13 +19,7 @@ namespace DataReaderDHT22
 
         static void Main(string[] args)
         {
-            // The Edge runtime gives us the connection string we need -- it is injected as an environment variable
-            string connectionString = Environment.GetEnvironmentVariable("EdgeHubConnectionString");
-
-            // Cert verification is not yet fully functional when using Windows OS for the container
-            bool bypassCertVerification = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-            if (!bypassCertVerification) InstallCert();
-            Init(connectionString, bypassCertVerification).Wait();
+            Init().Wait();
 
             // Wait until the app unloads or is cancelled
             var cts = new CancellationTokenSource();
@@ -45,49 +39,15 @@ namespace DataReaderDHT22
         }
 
         /// <summary>
-        /// Add certificate in local cert store for use by client for secure connection to IoT Edge runtime
+        /// Initializes the ModuleClient
         /// </summary>
-        static void InstallCert()
+        static async Task Init()
         {
-            string certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
-            if (string.IsNullOrWhiteSpace(certPath))
-            {
-                // We cannot proceed further without a proper cert file
-                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
-                throw new InvalidOperationException("Missing path to certificate file.");
-            }
-            else if (!File.Exists(certPath))
-            {
-                // We cannot proceed further without a proper cert file
-                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
-                throw new InvalidOperationException("Missing certificate file.");
-            }
-            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
-            store.Open(OpenFlags.ReadWrite);
-            store.Add(new X509Certificate2(X509Certificate2.CreateFromCertFile(certPath)));
-            Console.WriteLine("Added Cert: " + certPath);
-            store.Close();
-        }
-
-
-        /// <summary>
-        /// Initializes the DeviceClient and sets up the callback to receive
-        /// messages containing temperature information
-        /// </summary>
-        static async Task Init(string connectionString, bool bypassCertVerification = false)
-        {
-            Console.WriteLine("Connection String {0}", connectionString);
-
-            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
-            // During dev you might want to bypass the cert verification. It is highly recommended to verify certs systematically in production
-            if (bypassCertVerification)
-            {
-                mqttSetting.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
-            }
-            ITransportSettings[] settings = { mqttSetting };
+            AmqpTransportSettings amqpSetting = new AmqpTransportSettings(TransportType.Amqp_Tcp_Only);
+            ITransportSettings[] settings = { amqpSetting };
 
             // Open a connection to the Edge runtime
-            DeviceClient ioTHubModuleClient = DeviceClient.CreateFromConnectionString(connectionString, settings);
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
             await ioTHubModuleClient.OpenAsync();
             Console.WriteLine("IoT Hub module client initialized.");
 
@@ -108,7 +68,7 @@ namespace DataReaderDHT22
             return Task.CompletedTask;
         }
 
-        private static async Task SendData(DeviceClient deviceClient)
+        private static async Task SendData(ModuleClient deviceClient)
         {
             while (true)
             {
